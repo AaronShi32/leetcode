c9aa2b333909903987a30df9576bd5400e264c6c
岚岚 <Dameinv@Dameinv0901.lan>
2017-12-10 12:36:24 +0800

    init

---
+++ src/main/java/com/topic/array/EArrayPartitionI.java
@@ -0,0 +1,13 @@
+package com.topic.array;
+
+public class EArrayPartitionI {
+
+    static int solution(int[] nums){
+
+    }
+
+    public static void main(String[] args){
+        solution();
+    }
+
+}
