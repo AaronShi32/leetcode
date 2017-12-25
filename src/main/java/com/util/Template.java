9dbb5150455301963b001ef25e130de794535ccb
cunfeng.scf <cunfeng.scf@alibaba-inc.com>
2017-12-25 17:29:30 +0800

    15&123

---
+++ src/main/java/com/util/Template.java
@@ -0,0 +1,21 @@
+package com.util;
+
+@Question(id = 0, title = "", hint = "")
+public class Template {
+
+    @Best(thought = "", complexity = "")
+    static void leetcode(){
+
+    }
+
+    @Self(thought = "", complexity = "")
+    static void solution(){
+
+    }
+
+
+    public static void main(String[] args){
+
+        // System.out.println(solution());
+    }
+}
