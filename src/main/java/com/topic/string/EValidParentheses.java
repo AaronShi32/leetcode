092f66fee3b6b447c455a5f4030170a416efc96b
cunfeng.scf <cunfeng.scf@alibaba-inc.com>
2018-02-01 10:22:13 +0800

    update

--- src/main/java/com/topic/string/EValidParentheses.java
+++ src/main/java/com/topic/string/EValidParentheses.java
@@ -2,6 +2,27 @@ package com.topic.string;
 
 import com.util.Question;
 
+import java.util.Stack;
+
 @Question(id = 20, title = "Valid Parentheses")
 public class EValidParentheses {
+
+    public static boolean solution(String s){
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args){
+        System.out.print(solution("(){}[]((()]))"));
+    }
 }
