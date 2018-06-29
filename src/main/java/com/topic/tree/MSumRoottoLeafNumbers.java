2f8beb69271c3eece199d54c3a210419d941e2fc
晨升 <cunfeng.scf@alibaba-inc.com>
2018-06-29 10:11:19 +0800

    update

---
+++ src/main/java/com/topic/tree/MSumRoottoLeafNumbers.java
@@ -0,0 +1,20 @@
+package com.topic.tree;
+
+import com.support.TreeNode;
+import com.util.Question;
+
+@Question(id = 129, hint = "Find the total sum of all root-to-leaf numbers.")
+public class MSumRoottoLeafNumbers {
+
+    public int sumNumbers(TreeNode root) {
+        if(root == null){
+            return -1;
+        }
+        return sum(root.left) + sum(root.right);
+    }
+
+    private int sum(TreeNode node){
+        if()
+    }
+
+}
