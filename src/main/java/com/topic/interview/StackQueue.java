23ab2218662c23b866efd58f8268a61b0e11fd4b
晨升 <cunfeng.scf@alibaba-inc.com>
2018-07-06 10:42:48 +0800

    update

--- src/main/java/com/topic/interview/StackQueue.java
+++ src/main/java/com/topic/interview/StackQueue.java
@@ -1,7 +1,39 @@
 package com.topic.interview;
 
+import java.util.Stack;
+
 /**
  * 用两个栈来实现一个队列
  */
 public class StackQueue {
+
+    private Stack<Integer> s1 = new Stack<>();
+
+    private Stack<Integer> s2 = new Stack<>();
+
+    /**
+     * 入栈
+     * @param x
+     */
+    public void enqueue(int x){
+        s1.push(x);
+    }
+
+    /**
+     * 出栈
+     * @return
+     */
+    public int dequeue(){
+        // s2 不为空时不用倒, 直接用 s2.pop 即可
+        if(s2.empty()){
+            int count = s1.size();
+            while(count != 1){
+                s2.push(s1.pop());
+                count --;
+            }
+            return s1.pop();
+        } else {
+            return s2.pop();
+        }
+    }
 }
