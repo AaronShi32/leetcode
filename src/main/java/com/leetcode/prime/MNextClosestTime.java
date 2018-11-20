305421cf98717dd9be7aeb3f180b93ebac32091b
晨升 <cunfeng.scf@alibaba-inc.com>
2018-11-20 20:58:08 +0800

    update

--- src/main/java/com/leetcode/prime/MNextClosestTime.java
+++ src/main/java/com/leetcode/prime/MNextClosestTime.java
@@ -7,12 +7,12 @@ public class MNextClosestTime {
      * @param time
      * @return
      */
-    public String nextClosestTime(String time) {
+    public static String nextClosestTime(String time) {
         char[] t = time.toCharArray(), result = new char[4];
         // min
         int[] list = new int[10];
         char min = '9';
-        for (char c : t) {
+        for (char c : t) { // key step: get min digit
             if (c == ':') continue;
             list[c - '0']++;
             if (c < min) {
@@ -20,21 +20,21 @@ public class MNextClosestTime {
             }
         }
         //
-        for (int i = t[4] - '0' + 1; i <= 9; i++) {
+        for (int i = t[4] - '0' + 1; i <= 9; i++) { // 如果二分针在9以内可以走到, 直接返回
             if (list[i] != 0) {
                 t[4] = (char)(i + '0');
                 return new String(t);
             }
         }
-        t[4] = min;
-        for (int i = t[3] - '0' + 1; i <= 5; i++) {
+        t[4] = min; // key step: t[3,4] = min
+        for (int i = t[3] - '0' + 1; i <= 5; i++) { // 如果一分针在5以内可以走到, 直接返回
             if (list[i] != 0) {
                 t[3] = (char)(i + '0');
                 return new String(t);
             }
         }
         t[3] = min;
-        int stop = t[0] < '2' ? 9 : 3; // key step: hour
+        int stop = t[0] < '2' ? 9 : 3; // key step: // 根据一时针判断二时针的范围是9还是3
         for (int i = t[1] - '0' + 1; i <= stop; i++) {
             if (list[i] != 0) {
                 t[1] = (char)(i + '0');
@@ -52,4 +52,9 @@ public class MNextClosestTime {
         return new String(t);
     }
 
+
+    public static void main(String[] args){
+        // 一定是 next time
+        System.out.println(nextClosestTime("23:59"));
+    }
 }
